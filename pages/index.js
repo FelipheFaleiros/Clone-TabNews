d86de8d3b31679c1104e906b84400adf40d74cc6
FelipheDebian <feliphefaleiros@hotmail.com>
2024-08-16 08:54:26 -0700

    testando deploys da vercel

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
-    return <hi>Handebol eu gosto de jogar, se você gosta também da um risadinha :) 
-        Espero que não mude de url o Vercel
+    return <hi>Handebol eu gosto de jogar, se você gosta também da um risadinha :) !!
+        <br/>Espero que não mude de url o Vercel
     </hi>
 
 }
