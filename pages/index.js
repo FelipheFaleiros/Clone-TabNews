6d996bdc8e7f6a01d97bec99323655bc56068b31
FelipheDebian <feliphefaleiros@hotmail.com>
2024-08-16 08:48:39 -0700

    new commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home(){
-    return <hi>Handebol eu gosto de jogar, se você gosta também da um risadinha :) </hi>
+    return <hi>Handebol eu gosto de jogar, se você gosta também da um risadinha :) 
+        Espero que não mude de url o Vercel
+    </hi>
+
 }
 
 export default Home;
